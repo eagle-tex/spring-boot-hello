e4fdfa2b44c7ba642d94e5a7682224d2d9509aca
Gildas Montcho <gmontcho@gmail.com>
2025-01-11 10:34:23 +0100

    first commit - basic spring boot app

---
+++ src/test/java/com/personal/springboothello/SpringbootHelloApplicationTests.java
@@ -0,0 +1,11 @@
+package com.personal.springboothello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHelloApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
